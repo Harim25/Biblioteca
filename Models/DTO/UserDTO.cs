e44ee9a3268066c7a79c2ea3d0fec4f2af15d6ca
Isaid Aguiar <isaid.aguiar@gmail.com>
2024-11-25 01:42:40 -0600

    modificaciones respecto al CRUD de usuarios

---
+++ Models/DTO/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Models.DTO
+{
+    public class UserDTO
+    {
+        [Column("apellidos")]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Apellidos { get; set; } = null!;
+
+        [Column("edad")]
+        public int Edad { get; set; }
+
+        [Column("generoId")]
+        public int GeneroId { get; set; }
+
+        [Column("imagen")]
+        [StringLength(120)]
+        public string? Imagen { get; set; }
+    }
+}
