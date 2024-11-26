3e166461791de2aaded85595cd7f8f4b0a3f8b87
Isaid Aguiar <isaid.aguiar@gmail.com>
2024-11-25 20:11:02 -0600

    Funcionalidad parcial de Inicio

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Biblioteca.Models;
+using Biblioteca.Models.dbModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Biblioteca.Controllers
@@ -7,16 +9,20 @@ namespace Biblioteca.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BibliotecaContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BibliotecaContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var bibliotecaContext = _context.Libros.Include(l => l.Autor).Include(l => l.Genero);
+            return View(await bibliotecaContext.ToListAsync());
         }
+
         public ActionResult AccesoDenegado()
         {
             return View();
